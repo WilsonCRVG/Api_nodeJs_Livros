c4b46bef5ce624270801ff54168ae677eb582155
WilsonCRVGm <wilsoncrvg1994@gmail.com>
2023-06-24 19:07:55 -0300

    dotenv configurado e utilizando

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose"
 
-//const dbConfig = process.env.DB_CONFIG;
-const user = process.env.DB_USER;
-const password = process.env.DB_PASSWORD
+import dotenv from 'dotenv';
+dotenv.config();
 
-mongoose.connect("mongodb+srv://wilsoncrvg:1346798520@node.hbchpfr.mongodb.net/node-express");
-//"mongodb+srv://wilsoncrvg:1346798520@node.hbchpfr.mongodb.net/node-express"
-//"mongodb+srv://wilsoncrvg:1346798520@node.hbchpfr.mongodb.net/node-express"
+const dbConfig = process.env.DB_CONFIG;
+//const user = process.env.DB_USER;
+//const password = process.env.DB_PASSWORD
+
+mongoose.connect(dbConfig);
 
 let db = mongoose.connection;
 
 export default db;
-
